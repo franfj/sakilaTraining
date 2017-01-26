88fe4bf8518927cfe58d0709d8b00d26a24cd0d0
mrodort <leuzeugirdor>
2017-01-26 14:17:16 +0100

    feat: creacion parcial modelo customer & store

--- src/main/java/com/tsystems/sakila/domain/StoreEntity.java
+++ src/main/java/com/tsystems/sakila/domain/StoreEntity.java
@@ -1,5 +1,24 @@
 package com.tsystems.sakila.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class StoreEntity {
+
+  @Id
+  @Column(name = "store_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer storeId;
+
+  // TODO: mapear relaciones
+
+  @Column(name = "manager_staff_id")
+  private Integer managerStaffId;
+
+  @Column(name = "address_id")
+  private Integer addressId;
+
 
 }
