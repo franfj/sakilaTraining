a97cf0311aa7ea059f4388ebdb4a0173f98b57b2
TS-EU\jexposi1 <JOSE-ANGEL.Exposito@t-systems.com>
2017-01-26 12:39:22 +0100

    initial commit

---
+++ src/main/java/com/tsystems/sakila/SakilaTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.tsystems.sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaTrainingApplication.class, args);
+	}
+}
