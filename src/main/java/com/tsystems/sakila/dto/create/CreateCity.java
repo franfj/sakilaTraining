74611c8e0bf802edbd73104a0305a0adb0eebd38
TS-EU\jexposi1 <JOSE-ANGEL.Exposito@t-systems.com>
2017-01-27 11:58:52 +0100

    feat: creados paquetes update y create de las entidades city, country y address

---
+++ src/main/java/com/tsystems/sakila/dto/create/CreateCity.java
@@ -0,0 +1,10 @@
+package com.tsystems.sakila.dto.create;
+
+import lombok.Data;
+
+@Data
+public class CreateCity {
+
+  private String city;
+  private Long countryId;
+}
---
+++ src/main/java/com/tsystems/sakila/dto/create/CreateCountry.java
@@ -0,0 +1,9 @@
+package com.tsystems.sakila.dto.create;
+
+import lombok.Data;
+
+@Data
+public class CreateCountry {
+
+  private String country;
+}
