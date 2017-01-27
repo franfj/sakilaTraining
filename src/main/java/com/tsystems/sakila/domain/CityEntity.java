aae50adb4a222d09daef43d9a49bbbc92a0de634
TS-EU\jexposi1 <JOSE-ANGEL.Exposito@t-systems.com>
2017-01-27 09:26:34 +0100

    feat: mapeo de las entidades City, Country y Address

--- src/main/java/com/tsystems/sakila/domain/CityEntity.java
+++ src/main/java/com/tsystems/sakila/domain/CityEntity.java
@@ -1,8 +1,33 @@
 package com.tsystems.sakila.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
+@Table(name="city")
+@Data
+@ToString(of = { "id" })
+@EqualsAndHashCode(of = { "id" })
+
 public class CityEntity {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "city")
+    private String city;
+    
+    @Column(name = "country_id")
+    private Long countryId;
 
 }
--- src/main/java/com/tsystems/sakila/domain/CountryEntity.java
+++ src/main/java/com/tsystems/sakila/domain/CountryEntity.java
@@ -1,8 +1,30 @@
 package com.tsystems.sakila.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
+@Table(name="country")
+@Data
+@ToString(of = { "id" })
+@EqualsAndHashCode(of = { "id" })
+
 public class CountryEntity {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "country")
+    private String country;
+	
 
 }
