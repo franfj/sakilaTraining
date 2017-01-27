bd2d9a975157f95914dd00b25579c8eff334ddc1
Francisco Rodrigo <franjrg94@gmail.com>
2017-01-27 14:00:19 +0100

    fix: Usar findOne en vez de findById

--- src/main/java/com/tsystems/sakila/repositories/InventoryRepository.java
+++ src/main/java/com/tsystems/sakila/repositories/InventoryRepository.java
@@ -6,6 +6,4 @@ import com.tsystems.sakila.domain.InventoryEntity;
 
 public interface InventoryRepository extends JpaRepository<InventoryEntity, Integer> {
 
-  InventoryEntity findByInventoryId(Integer id);
-
 }
