ab67a1ec6e56bdd2e3be3a98e90fcbbfa1e80d97
Miguel Rodríguez Ortega <maysunderground@gmail.com>
2017-01-31 09:24:02 +0100

    fix: payment repo

--- src/main/java/com/tsystems/sakila/repositories/PaymentRepository.java
+++ src/main/java/com/tsystems/sakila/repositories/PaymentRepository.java
@@ -1,10 +1,14 @@
 package com.tsystems.sakila.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tsystems.sakila.domain.PaymentEntity;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Integer> {
 
-	PaymentEntity findById(Integer id);
+  @Query(value = "SELECT * FROM payment WHERE customerId = ? AND amount > 1", nativeQuery = true)
+  public List<PaymentEntity> findPaymentsByCustomerAndAmountGreaterThanOne(Integer customerId);
 }
